97c52ede724ec486300368e8083d5447263cf4f1
Mike Davies <mikedaviespp@gmail.com>
2021-09-02 13:06:36 +0100

    feat: added functions for templating

---
+++ funcs.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"path/filepath"
+	"strings"
+	"text/template"
+
+	"github.com/BurntSushi/toml"
+	"github.com/Masterminds/sprig/v3"
+	"gopkg.in/yaml.v2"
+)
+
+func funcMap(fileReader FileReader, ctx ApplicationContext) template.FuncMap {
+	f := sprig.TxtFuncMap()
+
+	extra := template.FuncMap{
+		"toToml":        toTOML,
+		"toYaml":        toYAML,
+		"fromYaml":      fromYAML,
+		"fromYamlArray": fromYAMLArray,
+		"toJson":        toJSON,
+		"fromJson":      fromJSON,
+		"fromJsonArray": fromJSONArray,
+		"fromFile":      fromFileWithCtx(fileReader, ctx),
+	}
+
+	for k, v := range extra {
+		f[k] = v
+	}
+
+	return f
+}
+
+func fromFileWithCtx(fileReader FileReader, ctx ApplicationContext) func(string) (string, error) {
+	return fromFile(fileReader, ctx.BaseFilePath())
+}
+
+// fromFile takes a file path and reads that file contents as a string
+//
+// called from template to load the contents of a file for further operation
+func fromFile(fileReader FileReader, baseDir string) func(string) (string, error) {
+
+	return func(path string) (string, error) {
+		file, err := fileReader.readFile(filepath.Join(baseDir, path))
+		if err != nil {
+			return "", err
+		}
+		return string(file.Data), nil
+	}
+
+}
+
+// ==================== FROM HELM REPO ====================
+// toYAML takes an interface, marshals it to yaml, and returns a string. It will
+// always return a string, even on marshal error (empty string).
+//
+// This is designed to be called from a template.
+func toYAML(v interface{}) string {
+	data, err := yaml.Marshal(v)
+	if err != nil {
+		// Swallow errors inside of a template.
+		return ""
+	}
+	return strings.TrimSuffix(string(data), "\n")
+}
+
+// fromYAML converts a YAML document into a map[string]interface{}.
+//
+// This is not a general-purpose YAML parser, and will not parse all valid
+// YAML documents. Additionally, because its intended use is within templates
+// it tolerates errors. It will insert the returned error message string into
+// m["Error"] in the returned map.
+func fromYAML(str string) map[string]interface{} {
+	m := map[string]interface{}{}
+
+	if err := yaml.Unmarshal([]byte(str), &m); err != nil {
+		m["Error"] = err.Error()
+	}
+	return m
+}
+
+// fromYAMLArray converts a YAML array into a []interface{}.
+//
+// This is not a general-purpose YAML parser, and will not parse all valid
+// YAML documents. Additionally, because its intended use is within templates
+// it tolerates errors. It will insert the returned error message string as
+// the first and only item in the returned array.
+func fromYAMLArray(str string) []interface{} {
+	a := []interface{}{}
+
+	if err := yaml.Unmarshal([]byte(str), &a); err != nil {
+		a = []interface{}{err.Error()}
+	}
+	return a
+}
+
+// toTOML takes an interface, marshals it to toml, and returns a string. It will
+// always return a string, even on marshal error (empty string).
+//
+// This is designed to be called from a template.
+func toTOML(v interface{}) string {
+	b := bytes.NewBuffer(nil)
+	e := toml.NewEncoder(b)
+	err := e.Encode(v)
+	if err != nil {
+		return err.Error()
+	}
+	return b.String()
+}
+
+// toJSON takes an interface, marshals it to json, and returns a string. It will
+// always return a string, even on marshal error (empty string).
+//
+// This is designed to be called from a template.
+func toJSON(v interface{}) string {
+	data, err := json.Marshal(v)
+	if err != nil {
+		// Swallow errors inside of a template.
+		return ""
+	}
+	return string(data)
+}
+
+// fromJSON converts a JSON document into a map[string]interface{}.
+//
+// This is not a general-purpose JSON parser, and will not parse all valid
+// JSON documents. Additionally, because its intended use is within templates
+// it tolerates errors. It will insert the returned error message string into
+// m["Error"] in the returned map.
+func fromJSON(str string) map[string]interface{} {
+	m := make(map[string]interface{})
+
+	if err := json.Unmarshal([]byte(str), &m); err != nil {
+		m["Error"] = err.Error()
+	}
+	return m
+}
+
+// fromJSONArray converts a JSON array into a []interface{}.
+//
+// This is not a general-purpose JSON parser, and will not parse all valid
+// JSON documents. Additionally, because its intended use is within templates
+// it tolerates errors. It will insert the returned error message string as
+// the first and only item in the returned array.
+func fromJSONArray(str string) []interface{} {
+	a := []interface{}{}
+
+	if err := json.Unmarshal([]byte(str), &a); err != nil {
+		a = []interface{}{err.Error()}
+	}
+	return a
+}
---
+++ funcs_test.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"path/filepath"
+	"strings"
+	"testing"
+	"text/template"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const testFile = "./afile"
+const baseDir = "/base"
+
+type testFileReader struct{}
+
+func (*testFileReader) readFile(path string) (*File, error) {
+	if path == filepath.Join(baseDir, testFile) {
+		return &File{Name: "hello", Data: []byte("goodbye")}, nil
+	} else {
+		return nil, fmt.Errorf("No file with path %s was found", path)
+	}
+}
+
+type testAppCtx struct{}
+
+func (*testAppCtx) BaseFilePath() string { return "/base" }
+
+func (*testAppCtx) Template() []byte { return []byte("random string") }
+
+func TestFuncs(t *testing.T) {
+	//TODO write tests for failure cases
+	tests := []struct {
+		tpl, expect string
+		vars        interface{}
+	}{{
+		tpl:    `{{ toYaml . }}`,
+		expect: `foo: bar`,
+		vars:   map[string]interface{}{"foo": "bar"},
+	}, {
+		tpl:    `{{ toToml . }}`,
+		expect: "foo = \"bar\"\n",
+		vars:   map[string]interface{}{"foo": "bar"},
+	}, {
+		tpl:    `{{ toJson . }}`,
+		expect: `{"foo":"bar"}`,
+		vars:   map[string]interface{}{"foo": "bar"},
+	}, {
+		tpl:    `{{ fromYaml . }}`,
+		expect: "map[hello:world]",
+		vars:   `hello: world`,
+	}, {
+		tpl:    `{{ fromYamlArray . }}`,
+		expect: "[one 2 map[name:helm]]",
+		vars:   "- one\n- 2\n- name: helm\n",
+	}, {
+		tpl:    `{{ fromYamlArray . }}`,
+		expect: "[one 2 map[name:helm]]",
+		vars:   `["one", 2, { "name": "helm" }]`,
+	}, {
+		// Regression for https://github.com/helm/helm/issues/2271
+		tpl:    `{{ toToml . }}`,
+		expect: "[mast]\n  sail = \"white\"\n",
+		vars:   map[string]map[string]string{"mast": {"sail": "white"}},
+	}, {
+		tpl:    `{{ fromYaml . }}`,
+		expect: "map[Error:yaml: unmarshal errors:\n  line 1: cannot unmarshal !!seq into map[string]interface {}]",
+		vars:   "- one\n- two\n",
+	}, {
+		tpl:    `{{ fromJson .}}`,
+		expect: `map[hello:world]`,
+		vars:   `{"hello":"world"}`,
+	}, {
+		tpl:    `{{ fromJson . }}`,
+		expect: `map[Error:json: cannot unmarshal array into Go value of type map[string]interface {}]`,
+		vars:   `["one", "two"]`,
+	}, {
+		tpl:    `{{ fromJsonArray . }}`,
+		expect: `[one 2 map[name:helm]]`,
+		vars:   `["one", 2, { "name": "helm" }]`,
+	}, {
+		tpl:    `{{ fromJsonArray . }}`,
+		expect: `[json: cannot unmarshal object into Go value of type []interface {}]`,
+		vars:   `{"hello": "world"}`,
+	}, {
+		tpl:    `{{ fromYaml . }}`,
+		expect: "map[Error:yaml: unmarshal errors:\n  line 1: cannot unmarshal !!seq into map[string]interface {}]",
+		vars:   `["one", "two"]`,
+	}, {
+		tpl:    `{{ fromYamlArray . }}`,
+		expect: "[yaml: unmarshal errors:\n  line 1: cannot unmarshal !!map into []interface {}]",
+		vars:   `hello: world`,
+	}, {
+		tpl:    "{{ fromFile . }}",
+		expect: "goodbye",
+		vars:   testFile,
+	}}
+
+	for _, tt := range tests {
+		var b strings.Builder
+		err := template.Must(template.New("test").Funcs(funcMap(&testFileReader{}, &testAppCtx{})).Parse(tt.tpl)).Execute(&b, tt.vars)
+		assert.NoError(t, err)
+		assert.Equal(t, tt.expect, b.String(), tt.tpl)
+	}
+}
+
+func TestWhenFileIsMissing_ThenFromFileFailsReturnsError(t *testing.T) {
+	tpl := "{{ fromFile . }}"
+	vars := "./nonExistantFile"
+
+	var b strings.Builder
+	err := template.Must(template.New("test").Funcs(funcMap(&testFileReader{}, &testAppCtx{})).Parse(tpl)).Execute(&b, vars)
+	if assert.Error(t, err) {
+		if ferr, ok := err.(template.ExecError); ok {
+			path := filepath.Join(baseDir, vars)
+			assert.Equal(t, fmt.Errorf("No file with path %s was found", path), errors.Unwrap(ferr.Unwrap()))
+		}
+	}
+}
